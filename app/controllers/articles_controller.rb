8e1ed8d4d14d84b797998d5d993b8a94a18e4036
Oscar M Olivera <oscarmolivera@gmail.com>
2019-12-28 12:06:21 -0300

    Leccion 22 - Pruebas de Controlador 1 y 2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
- def index; end
+ def index
+  articles = Article.all
+  render json: articles
+ end
 
  def show; end
 end
---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe ArticlesController, type: :controller do
+
+  describe "#index" do
+    subject {get :index}
+    
+    it 'deberia responder OK' do
+      subject
+      expect(response).to  have_http_status(:ok)
+    end
+
+    it "deberia responder un objeto JSON" do
+      subject
+      json = JSON.parse(response.body)
+      pp json # => impime la variable
+      #json_data = json['data']
+      #expect(json_data.length).to eq.(2)
+    end
+    
+  end
+  
+  
+end

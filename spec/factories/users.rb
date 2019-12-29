d300771a05ef5d5b3274c442dfd481e829b473c4
Oscar M Olivera <oscarmolivera@gmail.com>
2019-12-29 13:17:31 -0300

    Leccion 34 -Servicio de Authentication / Parte 2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:login) { |l| "usuario#{l}@login.com" }
-    name { "Nombre de Usuario" }
+    name { "John Snow" }
     url { "http://mytestapp.com" }
     avatar_url { "http://mytestapp.com/avatar.gif" }
     provider { "github" }
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -2,16 +2,45 @@ require 'rails_helper'
 
 RSpec.describe UserAuthenticator do
   describe "#perform:" do
-    context "cuando el codigo es incorrecto," do
 
+    let (:authenticator) {described_class.new('Some-sample-code')}
+    subject {authenticator.perform}
+    
+    context "cuando el código es incorrecto," do
+      let(:error){double("Sawyer::Resourse", error: 'bad_verification_code')}
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return(error)
+      end
+      
       it "debería mostrar un mensaje de ERROR." do
-        authenticator = described_class.new('Some-sample-code')
-        expect{authenticator.perform}.to raise_error(
+        expect{subject}.to raise_error(
           (UserAuthenticator::AuthenticationError)
         )
         expect(authenticator.user).to  be_nil
       end
-      
     end
-  end
+
+    context"cuando el código es CORRECTO," do
+      let(:user_data) do
+        {
+          login: 'usuario1@login.com',
+          url: 'http://mytestapp.com',
+          avatar_url: 'http://mytestapp.com/avatar.gif',
+          name: 'John Snow'
+        }
+      end
+        before do
+          allow_any_instance_of(Octokit::Client).to receive(
+            :exchange_code_for_token).and_return('validaccesstoken')
+  
+          allow_any_instance_of(Octokit::Client).to receive(
+            :user).and_return(user_data)
+        end
+  
+        it 'should save the user when does not exists' do
+          expect{ subject }.to change{ User.count }.by(1)
+          expect(User.last.name).to eq('John Snow')
+        end
+    end
+  end  
 end
